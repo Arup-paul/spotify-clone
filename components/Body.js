e4f7ebe72f4523a8a339e633326e4a0e2578c4a0
arup-paul <aruppaulctg@gmail.com>
2022-01-26 18:11:47 +0600

    auth

--- components/Body.js
+++ components/Body.js
@@ -1,11 +1,18 @@
 import Search from "./Search";
 import {useState} from "react";
+import {useSession} from "next-auth/react";
 
 const Body = () => {
+    const {data:session} = useSession();
+    const accessToken = session.accessToken;
     const [search,setSearch] = useState("")
+    const [searchResults,setSearchResults] = useState([])
     return (
         <section className="bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5 ">
            <Search search={search} setSearch={setSearch}/>
+            <div className="grid overflow-y-scroll scrollbar-hide h-96 py-4 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8">
+
+            </div>
         </section>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 import Head from 'next/head'
 import Dashboard from "../components/Dashboard";
+import {useRouter} from "next/router";
+import {useSession} from "next-auth/react";
+import Loader from "../components/Loader";
 export default function Home() {
+const router = useRouter();
+const {status,data:session} = useSession({
+    onUnauthenticated(){
+        router.push("/auth/signin")
+    }
+});
+
+if(status === "loading"){
+    return <Loader />;
+}
 
   return (
       <div>
