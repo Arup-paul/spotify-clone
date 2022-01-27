024e87a321d320a94077e39701edb6a12d75adc7
arup-paul <aruppaulctg@gmail.com>
2022-01-27 16:20:25 +0600

    responsive css added

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -16,7 +16,7 @@ const Dashboard = () => {
         setPlayingTrack(track)
     }
     return (
-        <main>
+        <main className="flex min-h-screen min-w-max bg-black lg:pb-24 ">
            <Sidebar />
             <Body spotifyApi={spotifyApi} chooseTrack={chooseTrack} />
             <Right />
